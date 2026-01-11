bfcba82eac76a38168c6178829f300b6f2450a4b
Andrej Bajusic <andrejbajusic2012@gmail.com>
2026-01-11 21:53:22 +0100

    Create morocco.py

---
+++ flags/perfectflags/asia/morocco.py
@@ -0,0 +1,21 @@
+from turtle import*
+
+color("#B7312C")
+begin_fill()
+for i in range (2):
+    forward(600)
+    right(90)
+    forward(400)
+    right(90)
+end_fill()
+forward(300)
+right(90)
+penup()
+forward(141.5)
+width(9.5)
+color("#006341")
+pendown()
+right(17)
+for i in range(5):
+    forward(117.6)
+    left(144)
